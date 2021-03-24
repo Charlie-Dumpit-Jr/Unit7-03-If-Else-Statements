fde03faec7660cb563459328c70074946ba5bcd8
CharlieDumpitJr <charlie.jr.dumpit@ocsbstudent.ca>
2021-03-24 13:43:17 +0000

    Unit7-03 If, Else Statements (2)

--- script.js
+++ script.js
@@ -5,17 +5,14 @@ let age = 0
 function testAge () {
   age = document.getElementById('input').value
   age = parseInt(age)
-  
   // check if user input is >=17, 13, 5.
   if (age >= 17) {
     document.getElementById('answer').innerHTML = 'You Can Watch Any Movies'
     alert('Thank You for Verifying your Age')
-  } 
-  else if (age >= 13) {
+  } else if (age >= 13) {
     document.getElementById('answer').innerHTML = 'You can Watch a PG 13 or a G Movie'
     alert('Thank You for Verifying your Age')
-  }
-  else if (age >=5) {
+  } else if (age >= 5) {
     document.getElementById('answer').innerHTML = 'You are too Young'
     alert('Thank You for Verifying your Age')
   }
